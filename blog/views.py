253bbde32d0b248ad3c2fa55b5b60bf93e9b65c3
Homero00 <assasin.matias@gmail.com>
2022-07-20 02:57:41 -0300

    despliegue css y añadiendo enlace url a los link

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from time import time
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -9,3 +8,7 @@ def post_list(request):
     #This is show in the portal with a specific <div> in html text 'post_list.html' 
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
