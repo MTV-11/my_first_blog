9a50d1d93b66378df7d0053470a031a06d7e03e3
Homero00 <assasin.matias@gmail.com>
2022-07-20 00:20:25 -0300

    Publicación de post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
+from time import time
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #Use QuerySet and save filter in a variable
+    #This is show in the portal with a specific <div> in html text 'post_list.html' 
+    posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
